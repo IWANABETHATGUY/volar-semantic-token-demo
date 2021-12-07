06855057ee311419b6af7de57e7675064fdddb95
hxj <hexiangjun@bytedance.com>
2021-12-07 20:11:39 +0800

    feat: 🎸 MVP

--- sample/index.vue
+++ sample/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="settings-main card">
-        <a-input title="标题" >
-            <template #default={row}>
+        <a-input title="标题" @default.capture.left="test">
+            <template #default={test}>
             </template>
         </a-input>
     </div>
